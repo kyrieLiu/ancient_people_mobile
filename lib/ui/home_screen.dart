c5f0786f6802bb41c5d81ef6d57ad2bd8e8a6817
15153246027 <652992429@qq.com>
2020-06-16 22:24:37 +0800

    优化启动页,调整图片

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:ancientpeoplemobile/common/common.dart';
 import 'package:ancientpeoplemobile/data/api/apis_service.dart';
 import 'package:ancientpeoplemobile/data/model/article_model.dart';
 import 'package:ancientpeoplemobile/data/model/banner_model.dart';
-import 'package:ancientpeoplemobile/ui/base_widget.dart';
 import 'package:ancientpeoplemobile/utils/route_util.dart';
 import 'package:ancientpeoplemobile/utils/toast_util.dart';
 import 'package:ancientpeoplemobile/widgets/ItemArticleList.dart';
@@ -13,13 +12,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class HomeScreen extends BaseWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return super.createState();
-  }
+import 'base_widget.dart';
 
+/// 首页
+class HomeScreen extends BaseWidget {
   @override
   BaseWidgetState<BaseWidget> attachState() {
     return HomeScreenState();
@@ -27,19 +23,19 @@ class HomeScreen extends BaseWidget {
 }
 
 class HomeScreenState extends BaseWidgetState<HomeScreen> {
-  //首页轮播图
+  /// 首页轮播图数据
   List<BannerBean> _bannerList = new List();
 
-  //首页文章列表数据
+  /// 首页文章列表数据
   List<ArticleBean> _articles = new List();
 
-  //listView控制器
+  /// listview 控制器
   ScrollController _scrollController = new ScrollController();
 
-  //是否显示悬浮按钮
+  /// 是否显示悬浮按钮
   bool _isShowFAB = false;
 
-  //页码
+  /// 页码，从0开始
   int _page = 0;
 
   RefreshController _refreshController =
@@ -55,13 +51,18 @@ class HomeScreenState extends BaseWidgetState<HomeScreen> {
   void didChangeDependencies() {
     super.didChangeDependencies();
     _bannerList.add(null);
+
     showLoading().then((value) {
       getBannerList();
       getTopArticleList();
     });
+
     _scrollController.addListener(() {
+      /// 滑动到底部，加载更多
       if (_scrollController.position.pixels ==
-          _scrollController.position.maxScrollExtent) {}
+          _scrollController.position.maxScrollExtent) {
+        // getMoreArticleList();
+      }
       if (_scrollController.offset < 200 && _isShowFAB) {
         setState(() {
           _isShowFAB = false;
@@ -74,24 +75,23 @@ class HomeScreenState extends BaseWidgetState<HomeScreen> {
     });
   }
 
+  /// 获取轮播数据
   Future getBannerList() async {
     apiService.getBannerList((BannerModel bannerModel) {
       if (bannerModel.data.length > 0) {
-        showContent().then((value) => {
-              setState(() {
-                _bannerList = bannerModel.data;
-              })
-            });
+        setState(() {
+          _bannerList = bannerModel.data;
+        });
       }
     });
   }
 
-  ///获取指定文章
+  /// 获取置顶文章数据
   Future getTopArticleList() async {
     apiService.getTopArticleList((TopArticleModel topArticleModel) {
       if (topArticleModel.errorCode == Constants.STATUS_SUCCESS) {
-        topArticleModel.data.forEach((element) {
-          element.top = 1;
+        topArticleModel.data.forEach((v) {
+          v.top = 1;
         });
         _articles.clear();
         _articles.addAll(topArticleModel.data);
@@ -102,26 +102,26 @@ class HomeScreenState extends BaseWidgetState<HomeScreen> {
     });
   }
 
+  /// 获取文章列表数据
   Future getArticleList() async {
     _page = 0;
-    apiService.getArticleList(
-        (ArticleModel model) => {
-              if (model.errorCode == Constants.STATUS_SUCCESS)
-                {
-                  if (model.data.datas.length > 0)
-                    {
-                      showContent().then((value) {
-                        _refreshController.refreshCompleted(
-                            resetFooterState: true);
-                        setState(() {
-                          _articles.addAll(model.data.datas);
-                        });
-                      })
-                    }
-                  else
-                    {showEmpty()}
-                }
-            }, (DioError error) {
+    apiService.getArticleList((ArticleModel model) {
+      if (model.errorCode == Constants.STATUS_SUCCESS) {
+        if (model.data.datas.length > 0) {
+          showContent().then((value) {
+            _refreshController.refreshCompleted(resetFooterState: true);
+            setState(() {
+              _articles.addAll(model.data.datas);
+            });
+          });
+        } else {
+          showEmpty();
+        }
+      } else {
+        showError();
+        T.show(msg: model.errorMsg);
+      }
+    }, (DioError error) {
       showError();
     }, _page);
   }
@@ -149,9 +149,13 @@ class HomeScreenState extends BaseWidgetState<HomeScreen> {
   }
 
   @override
+  AppBar attachAppBar() {
+    return AppBar(title: Text(""));
+  }
+
+  @override
   Widget attachContentWidget(BuildContext context) {
     return Scaffold(
-      // body: Container(child: Text("aaaaa")),
       body: SmartRefresher(
         enablePullDown: true,
         enablePullUp: true,
@@ -173,25 +177,36 @@ class HomeScreenState extends BaseWidgetState<HomeScreen> {
               heroTag: "home",
               child: Icon(Icons.arrow_upward),
               onPressed: () {
+                /// 回到顶部时要执行的动画
                 _scrollController.animateTo(0,
-                    duration: Duration(milliseconds: 1000), curve: Curves.ease);
+                    duration: Duration(milliseconds: 2000), curve: Curves.ease);
               },
             ),
     );
   }
 
+  @override
+  void onClickErrorWidget() {
+    showLoading().then((value) {
+      getBannerList();
+      getTopArticleList();
+    });
+  }
+
+  /// ListView 中每一行的视图
   Widget itemView(BuildContext context, int index) {
     if (index == 0) {
       return Container(
-          height: 200, color: Colors.transparent, child: _buildBannerWidget());
+        height: 200,
+        color: Colors.transparent,
+        child: _buildBannerWidget(),
+      );
     }
     ArticleBean item = _articles[index - 1];
-    //return Item
-    return ItemArticleList(
-      item: item,
-    );
+    return ItemArticleList(item: item);
   }
 
+  /// 构建轮播视图
   Widget _buildBannerWidget() {
     return Offstage(
       offstage: _bannerList.length == 0,
@@ -203,10 +218,9 @@ class HomeScreenState extends BaseWidgetState<HomeScreen> {
             return new Container(height: 0);
           } else {
             return InkWell(
-              child: Container(
-                child: CustomCachedImage(
-                  imageUrl: _bannerList[index].imagePath,
-                ),
+              child: new Container(
+                child:
+                    CustomCachedImage(imageUrl: _bannerList[index].imagePath),
               ),
               onTap: () {
                 RouteUtil.toWebView(
@@ -218,6 +232,7 @@ class HomeScreenState extends BaseWidgetState<HomeScreen> {
         itemCount: _bannerList.length,
         autoplay: true,
         pagination: new SwiperPagination(),
+        // control: new SwiperControl(),
       ),
     );
   }
@@ -227,19 +242,5 @@ class HomeScreenState extends BaseWidgetState<HomeScreen> {
     _refreshController.dispose();
     _scrollController.dispose();
     super.dispose();
-  }
-
-//  @override
-//  AppBar attachAppBar() {
-//    return  Container();
-//  }
-  @override
-  AppBar attachAppBar() {
-    return AppBar(title: Text(""));
-  }
-
-  @override
-  void onClickErrorWidget() {
-    showLoading().then((value) => {getBannerList()});
   }
 }
